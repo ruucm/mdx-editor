396a8aa415b739c59fc144d8c741c94c9251fcee
ruucm <ruucm.a@gmail.com>
2021-08-14 19:01:21 +0900

    add useESM

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useESM } from "../hooks/use-esm";
 
 export default IndexPage;
 
 function IndexPage() {
+  const m: any = useESM("https://module.harbor.school/m/useScrollDirection.js");
+  console.log("m?.useScrollDirection}", m?.useScrollDirection);
+
   return <>Home</>;
 }
