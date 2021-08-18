05a8fe72c82f377cea01aacd2524c7f84f53607a
ruucm <ruucm.a@gmail.com>
2021-08-18 13:31:19 +0900

    editing (now)

--- pages/editor/editor.tsx
+++ pages/editor/editor.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from "react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
-import { SOCKET_PORT } from "../../consts";
-import { MySetup } from "../_components/TreeHSortable/Tree.story";
-import { isClient } from "../../utils";
-import { useAtom } from "jotai";
-import { mdxItemsAtom } from "../../store";
+import { useAtom } from "jotai"
+import React, { useEffect } from "react"
+import useWebSocket, { ReadyState } from "react-use-websocket"
+import { SOCKET_PORT, testMdxFileBase, testMdxFileName } from "../../consts"
+import { mdxItemsAtom } from "../../store"
+import { isClient } from "../../utils"
+import { MySetup } from "../_components/TreeHSortable/Tree.story"
 
 export function Page({ list }: any) {
-  const hostname = typeof window !== "undefined" && window.location.hostname;
-  const socketUrl = `ws://${hostname}:${SOCKET_PORT}`;
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+  const hostname = typeof window !== "undefined" && window.location.hostname
+  const socketUrl = `ws://${hostname}:${SOCKET_PORT}`
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl)
 
   const connectionStatus = {
     [ReadyState.CONNECTING]: "Connecting",
@@ -17,22 +17,24 @@ export function Page({ list }: any) {
     [ReadyState.CLOSING]: "Closing",
     [ReadyState.CLOSED]: "Closed",
     [ReadyState.UNINSTANTIATED]: "Uninstantiated",
-  }[readyState];
+  }[readyState]
 
   useEffect(() => {
-    console.log("lastMessage", lastMessage);
-  }, [lastMessage]);
+    console.log("lastMessage", lastMessage)
+  }, [lastMessage])
 
   // sync written mdx content
-  const [, setMdxItems] = useAtom(mdxItemsAtom);
+  const [, setMdxItems] = useAtom(mdxItemsAtom)
   useEffect(() => {
-    setMdxItems(list);
-  }, [list]);
+    setMdxItems(list)
+  }, [list])
 
   return (
     <>
       connectionStatus : {connectionStatus}
+      <br />
+      editing (now) : {`${testMdxFileBase}${testMdxFileName}`}
       {isClient && <MySetup sendMessage={sendMessage} />}
     </>
-  );
+  )
 }
